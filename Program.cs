48a777a644faf22367b5bc70c0b052b0ce371711
Thiago Christopher <thiago_cfc@hotmail.com>
2020-08-25 20:48:46 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace dsd_json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserModel user = new UserModel("Leonan", 21, 10.002);
+
+            string json = JsonConvert.SerializeObject(user);
+
+            Console.WriteLine(json);
+
+            var usuarioFromJson = JsonConvert.DeserializeObject<UserModel>(json);
+
+            Console.Write(usuarioFromJson.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dsd_json
+{
+    public class UserModel
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public double Salary { get; private set; }
+
+        public UserModel(string name, int age, double salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome = { Name }, Age = { Age }, Salary = { Salary }";
+        }
+    }
+}
